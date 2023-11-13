b7033dd415c5573572fcff93288a07cd86c03ec0
annov4 <137298843+annov4@users.noreply.github.com>
2023-11-13 17:44:49 +0300

    final 3.1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,15 @@
 public class Calculator {
     public static double fireCalculator(int startYear) {
         int indexYear = startYear - 2002;
-        double maxWithdrawalPercent = 0;
+        double maxWithdrawalPercent = 0.5;
         double capital = 1;
         while (indexYear <= 20) {
             double stockIndex = calculateStockIndex(indexYear);
             double inflationRate = calculateInflationRate(indexYear);
             double baseExpenses = capital * maxWithdrawalPercent / 100;
-            double currentCapital = calculateCurrentCapital(capital,stockIndex,baseExpenses);
             double updatedBaseExpenses = calculateUpdatedBaseExpenses(baseExpenses, inflationRate);
+            double currentCapital = calculateCurrentCapital(capital,stockIndex,baseExpenses);
+
 
             if (currentCapital > 0) {
                 maxWithdrawalPercent += 0.5;
@@ -23,7 +24,6 @@ public class Calculator {
     }
 
     private static double calculateStockIndex(int indexYear) {
-        //расчет индекса московской биржи на текущий год
         double stockIndex = (Constants.MOEX_RATE[indexYear] - Constants.MOEX_RATE[indexYear - 1]) / Constants.MOEX_RATE[indexYear - 1];
         return stockIndex;
     }
@@ -37,6 +37,7 @@ public class Calculator {
         double updatedBaseExpenses = baseExpenses * (1 + inflationRate / 100);
         return updatedBaseExpenses;
     }
+
     private static double calculateCurrentCapital(double capital, double stockIndex, double updatedBaseExpenses) {
         double currentCapital = capital * (1 + stockIndex) - updatedBaseExpenses;
         return currentCapital;
